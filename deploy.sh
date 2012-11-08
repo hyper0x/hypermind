cc639eeb797c5e3d502e211cfd67980f39fcf10d
Harry Hao <freej.cn@gmail.com>
2012-11-08 16:34:06 +0800

    upgrade deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-PID=`ps -ef | grep "go-web-demo" | grep -v grep|awk '{print $2}'`
+PID=`ps -ef | grep "go-web-demo" | grep "server" | grep -v grep | awk '{print $2}'`
 kill $PID
 echo "The go-web-demo has stopped!"
-./go-web-demo 2>&1 | cronolog "go-web-demo.log.%Y-%m-%d" &
+go build server
+chmod +rx server
+DIR=`pwd`
+$DIR/server 2>&1 | cronolog "go-web-demo.log.%Y-%m-%d" &
 echo "The go-web-demo has been launched."
